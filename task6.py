3a50ad5b54c5ac22edd6eea6d09a948bfcc58ec2
Иван Казаков <146557605+Galaritus@users.noreply.github.com>
2023-10-07 16:22:44 +0000

    первая лаба

---
+++ task6.py
@@ -0,0 +1,7 @@
+a = []
+a.append(input('Возраст пользователя'))
+a.append(input('Пол пользователя'))
+a.append(input('Имя пользователя'))
+a.append(input('Город пользователя'))
+a.append(input('Класс пользователя'))
+print(a) 
